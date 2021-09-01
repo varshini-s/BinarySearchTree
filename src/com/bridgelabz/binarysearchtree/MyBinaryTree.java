d4155d0f67779be7a33ba88786e4c68e4ea8af67
varshini <dreamvarshini@gmail.com>
2021-09-01 15:27:54 +0530

    ADDED:function to add elements to binary search tree

---
+++ src/com/bridgelabz/binarysearchtree/MyBinaryTree.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.binarysearchtree;
+
+public class MyBinaryTree<K extends Comparable<K>>
+
+{
+	private MyBinaryNode<K> root;
+	
+	public void add(K key)
+	{
+		this.root=this.addRecursively(root,key);
+	}
+
+	private MyBinaryNode<K> addRecursively(MyBinaryNode<K> current, K key) 
+	
+	{
+		
+		if(current==null)
+			return new MyBinaryNode<>(key);
+		int compareResult=key.compareTo(current.key);
+		if (compareResult==0) return current;
+		if(compareResult<0)
+		{
+			current.left=addRecursively(current.left,key);
+		}
+		else
+		{
+			current.right=addRecursively(current.right, key);
+		}
+		return current;
+	}
+	
+
+}
